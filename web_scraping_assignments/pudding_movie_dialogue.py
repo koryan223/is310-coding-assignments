d3049c29134367eee7830297453558989086dc69
Kimbap <baboo@wirelessprv-10-195-174-246.near.illinois.edu>
2024-02-22 12:48:09 -0600

    final fix

--- web_scraping_assignments/pudding_movie_dialogue.py
+++ web_scraping_assignments/pudding_movie_dialogue.py
@@ -17,23 +17,22 @@ def scrape_dialogue(url):
         dialogue_text = ' '.join(p.get_text() for p in soup.find_all('p'))
         return dialogue_text[:1000]
 
-if __name__ == "__main__":
-    input_file_path = 'cleaned_pudding_data.csv' 
-    output_file_path = 'pudding_movie_dialogue.csv'
+input_file_path = 'cleaned_pudding_data.csv' 
+output_file_path = 'pudding_movie_dialogue.csv'
 
-    urls = load_urls(input_file_path)
-    results = []
+urls = load_urls(input_file_path)
+results = []
 
-    for url in urls:
-        print(f'Scraping {url}...')
-        dialogue = scrape_dialogue(url)
-        results.append({'url': url, 'dialogue_snippet': dialogue})
-        print(f'Done with {url}')
+for url in urls:
+    print(f'Scraping {url}...')
+    dialogue = scrape_dialogue(url)
+    results.append({'url': url, 'dialogue_snippet': dialogue})
+    print(f'Done with {url}')
 
-    with open(output_file_path, mode='w', newline='', encoding='utf-8') as file:
-        fieldnames = ['url', 'dialogue_snippet']
-        writer = csv.DictWriter(file, fieldnames=fieldnames)
-        writer.writeheader()
-        for result in results:
-            writer.writerow(result)
+with open(output_file_path, mode='w', newline='', encoding='utf-8') as file:
+    fieldnames = ['url', 'dialogue_snippet']
+    writer = csv.DictWriter(file, fieldnames=fieldnames)
+    writer.writeheader()
+    for result in results:
+        writer.writerow(result)
 
